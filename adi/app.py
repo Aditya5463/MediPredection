01530b534b8b82a79154c2fa8cb90a9e1183c845
Aditya5463 <161203105+Aditya5463@users.noreply.github.com>
2025-08-31 18:00:00 +0530

    Update app.py

--- adi/app.py
+++ adi/app.py
@@ -388,5 +388,5 @@ def history():
 # Run
 # ----------------------------------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
+    # You can also set host="0.0.0.0" for LAN testing
+    app.run(debug=True)
